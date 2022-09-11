fb4d50fcaadc3233035898e5beb6c88b363bc914
ParasPandey019 <paraspandey019@gmail.com>
2022-09-11 13:12:21 +0530

    added template directory and paths in settings

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Returning a Http response
+# def index(request):
+#     return HttpResponse("index Http Response from first_app/views")
+
+
+
+# Returning and rendering a template as a response
+def index(request):
+    my_dict ={'insert_this':"This is from views.py!"}
+    return render(request, 'first_app/index.html', context=my_dict)
